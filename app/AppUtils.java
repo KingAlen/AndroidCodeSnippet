1801033bc43597037c360037b19ebd89255ff43b
jkwangbest <jkwangbest@gmail.com>
2017-01-01 23:48:32 +0800

    App相关工具，获取当前的versionName

---
+++ app/AppUtils.java
@@ -0,0 +1,25 @@
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+public class AppUtils {
+
+
+    /**
+     * 获取当前版本号
+     *
+     * @param context 上下文
+     * @return 版本号
+     */
+    public static String getVersionName(Context context) {
+        String versionName = "";
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo("com.package.name", 0);
+            versionName = packageInfo.versionName;
+        } catch (Exception e) {
+            versionName = "";
+        }
+        return versionName;
+    }
+}
